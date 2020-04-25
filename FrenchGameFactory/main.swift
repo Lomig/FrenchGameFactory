f233d2d74462fb59f00b867ef60940840a59f843
Lomig <dunwael@gmail.com>
2020-04-25 16:20:46 +0200

    Initial Commit

---
+++ FrenchGameFactory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FrenchGameFactory
+//
+//  Created by Lomig Enfroy on 25/04/2020.
+//  Copyright © 2020 Lomig Enfroy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
